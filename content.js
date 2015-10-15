df2389feed4e109fc13f06e78e68666e1330d8d5
Josh Holtz <josh@rokkincat.com>
2015-10-15 08:53:31 -0500

    Spaces because yeah

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-	// Gets branch name form document
-	var branch = document.getElementsByClassName("current-branch")[1].children[0].innerHTML;
-	msg["branch"] = branch
-	sendResponse(msg);
+  // Gets branch name form document
+  var branch = document.getElementsByClassName("current-branch")[1].children[0].innerHTML;
+  msg["branch"] = branch
+  sendResponse(msg);
 });
--- options.js
+++ options.js
@@ -11,25 +11,25 @@ function isJsonString(str) {
 // Saves options to chrome.storage.sync
 function save_options() {
   var config = document.getElementById('config').value;
-	if (isJsonString(config)) {
-	  chrome.storage.sync.set({
-	    config: config,
-	  }, function() {
-	    // Update status to let user know options were saved.
-	    var status = document.getElementById('status');
-	    status.textContent = 'Options saved.';
-	    setTimeout(function() {
-	      status.textContent = '';
-	    }, 750);
-	  });
-	} else {
-		// Update status to let user know options were saved.
-		var status = document.getElementById('status');
-		status.textContent = 'Invalid JSON.';
-		setTimeout(function() {
-			status.textContent = '';
-		}, 750);
-	}
+  if (isJsonString(config)) {
+    chrome.storage.sync.set({
+      config: config,
+    }, function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+  } else {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Invalid JSON.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  }
 }
 
 // Restores default options for config
