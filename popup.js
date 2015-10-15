f373d65c2d378bfc062ea0f1a892688962c430c5
Josh Holtz <josh@rokkincat.com>
2015-10-15 16:09:04 -0500

    This works but this code is so ugly and needs to get cleaned up

--- popup.js
+++ popup.js
@@ -16,11 +16,11 @@ function getCurrentTabUrl(callback) {
 function sendRequest(url, method, body, callback, errorCallback) {
   var x = new XMLHttpRequest();
   x.open(method, url);
-  x.setRequestHeader('Accept', 'application/json');
-  x.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    x.setRequestHeader('Accept', 'application/json');
+    x.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   x.onreadystatechange = function() {
     if (x.readyState == 4) {
-      if (x.status == 201) {
+      if (x.status >= 200 && x.status < 300) {
         callback(x);
       } else {
         errorCallback(x);
@@ -30,13 +30,24 @@ function sendRequest(url, method, body, callback, errorCallback) {
   x.onerror = function() {
     errorCallback('Network error.');
   };
-  x.send(JSON.stringify(body));
+  if (body) {
+    console.log("Sending body");
+    x.send(JSON.stringify(body));
+  } else {
+    console.log("Sending no body");
+    x.send();
+  }
 }
 
 function renderStatus(statusText) {
   document.getElementById('status').innerHTML = statusText;
 }
 
+function renderBuilds(buildsHTML) {
+  document.getElementById('builds').innerHTML = buildsHTML;
+}
+
+var githubPullRequestUrl = null;
 var owner = null;
 var repo = null;
 var pull = null;
@@ -44,13 +55,35 @@ var pull = null;
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url, tab) {
 
+    githubPullRequestUrl = url;
+
+    var el = document.createElement('a');
+    el.href = url;
+    var parts = el.pathname.split("/");
+
+    console.log("parts", parts);
+
+    if (parts.length >= 3) {
+      owner = parts[1];
+      repo = parts[2];
+
+      console.log("owner b", owner);
+      console.log("repo b", repo);
+
+      setTimeout(function() {
+        chrome.tabs.sendMessage(tab.id, {}, showListOfBuilds);
+      }, 1000);
+    } else {
+      console.log("doesn't any github anything")
+    }
+
     // Verify that we are on a github pull request with regex on URL
-    var regex = /github.com\/(.*)\/(.*)\/pull\/([0-9]+)/;
-    var matches = regex.exec(url);
-    if (matches && matches.length == 4) {
-      owner = matches[1];
-      repo = matches[2];
-      pull = matches[3];
+    if (parts.length >= 5 && parts[3] === "pull") {
+      pull = parts[4];
+
+      console.log("owner", owner);
+      console.log("repo", repo);
+      console.log("pull", pull);
 
       var button = "<button id='kickOffButton'>Make build for PR " + pull + "</button>"
       var textfield = "<input id='parameters' type='text' placeholder='Build params as JSON (optional)'></intput"
@@ -64,8 +97,162 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
       renderStatus('Cannot create a build from this page');
     }
+
   });
 });
+
+function showListOfBuilds(message) {
+
+  var branch = message.branch
+  console.log("Branch", branch);
+
+  var circleToken = null;
+
+  chrome.storage.sync.get({
+    config: '',
+  }, function(items) {
+    var config = items.config || "{}";
+    var obj = JSON.parse(config);
+    var circleToken = obj[owner + "/" + repo];
+    if (!circleToken) {
+      renderStatus("No CircleCI API token found for: " + owner + "/" + repo);
+      return;
+    }
+
+    /*
+     * Look for current builds
+     */
+    var circleURL = "https://circleci.com/api/v1/project/" + owner + "/" + repo + "?shallow=true&offset=0&limit=30&circle-token=" + circleToken;
+    if (branch) {
+      circleURL = "https://circleci.com/api/v1/project/" + owner + "/" + repo +"/tree/" + branch + "?shallow=true&offset=0&limit=30&circle-token=" + circleToken;
+    }
+    sendRequest(circleURL, 'GET', null, function(response) {
+
+      var buildsForThisPR = []
+
+      var obj = JSON.parse(response.responseText);
+      for (var key in obj) {
+        var build = obj[key]
+        var pullRequestUrls = build["pull_request_urls"];
+
+        var indexOfPR = pullRequestUrls.indexOf(githubPullRequestUrl)
+        if (indexOfPR > -1 || pull === null) {
+          buildsForThisPR.push(build)
+        }
+      }
+
+      var listeners = [];
+
+      var buildsHTML = ""
+
+      buildsHTML += "<table><tbody>";
+      buildsHTML += "<tr><th>#</th><th>Status</th><th>Outcome</th><th colspan=2>Action</th></tr>"
+      for (var key in obj) {
+        var build = obj[key]
+
+        var status = build["status"];
+        var startTime = build["start_time"];
+        var outcome = build["outcome"];
+        var buildUrl = build["build_url"];
+        var buildNum = build["build_num"];
+        var pullRequestUrls = build["pull_request_urls"];
+        var subject = build["subject"];
+
+        if (outcome === null) {
+          outcome = "running";
+        }
+
+        var viewButtonID = "btn-view-" + buildNum
+        var cancelButtonID = "btn-cancel-" + buildNum
+
+        buildsHTML += "<tr>";
+        buildsHTML += "<td>" + buildNum + "</td>";
+        buildsHTML += "<td>" + status + "</td>";
+        buildsHTML += "<td>" + outcome + "</td>";
+        if (outcome === "running") {
+          buildsHTML += "<td><button id='" + viewButtonID + "' type='button' text='View'>View</button></td>";
+          buildsHTML += "<td><button id='" + cancelButtonID + "' type='button' text='Cancel'>Cancel</button></td>";
+        } else {
+          buildsHTML += "<td><button id='" + viewButtonID + "' type='button' text='View')'>View</button></td>";
+        }
+        buildsHTML += "</tr>";
+
+
+      }
+      buildsHTML += "</tbody></table>";
+      renderBuilds(buildsHTML);
+
+      setTimeout(function() {
+        for (var key in obj) {
+          (function() {
+            var build = obj[key]
+            var buildNum = build["build_num"];
+
+            var viewButtonID = "btn-view-" + buildNum
+            var cancelButtonID = "btn-cancel-" + buildNum
+
+            var viewBtn = document.getElementById(viewButtonID);
+            var cancelBtn = document.getElementById(cancelButtonID);
+
+            if (viewBtn) {
+              viewBtn.addEventListener('click', function() {
+                openCircleBuild(build);
+              });
+            }
+
+            if (cancelBtn) {
+              cancelBtn.addEventListener('click', function() {
+                cancelCircleBuild(build)
+              });
+            }
+          }());
+
+        }
+      }, 250);
+
+    }, function(errorMessage) {
+
+    });
+
+  });
+
+}
+
+function openCircleBuild(build) {
+  var buildUrl = build["build_url"];
+  chrome.tabs.create({ url: buildUrl });
+}
+
+function cancelCircleBuild(build) {
+
+  var buildUrl = build["build_url"];
+  var owner = build["username"];
+  var repo = build["reponame"];
+  var branch = build["branch"];
+  var buildNum = build["build_num"];
+
+  var circleToken = null;
+
+  chrome.storage.sync.get({
+    config: '',
+  }, function(items) {
+    var config = items.config || "{}";
+    var obj = JSON.parse(config);
+    var circleToken = obj[owner + "/" + repo];
+    if (!circleToken) {
+      renderStatus("No CircleCI API token found for: " + owner + "/" + repo);
+      return;
+    }
+
+    var circleURL = "https://circleci.com/api/v1/project/" + owner + "/" + repo +"/" + buildNum + "/cancel?circle-token=" + circleToken;
+    sendRequest(circleURL, 'POST', null, function(response) {
+      chrome.tabs.create({ url: buildUrl });
+    });
+
+  });
+
+
+}
 
 /* A function creator for callbacks */
 function doStuffWithDOM(message) {
